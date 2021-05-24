0b276a99c4a764dc1fc6d1892950b65a32daf5e7
mhmroichan <84715308+mhmroichan@users.noreply.github.com>
2021-05-24 20:43:52 +0800

    Update pra_nikah.sql

--- database/pra_nikah.sql
+++ database/pra_nikah.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
--- https://www.phpmyadmin.net/
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 12 Jun 2020 pada 01.51
--- Versi server: 10.1.31-MariaDB
--- Versi PHP: 5.6.34
+-- Generation Time: May 24, 2021 at 02:43 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -25,7 +23,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `admin`
+-- Table structure for table `admin`
 --
 
 CREATE TABLE `admin` (
@@ -35,135 +33,32 @@ CREATE TABLE `admin` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data untuk tabel `admin`
+-- Dumping data for table `admin`
 --
 
 INSERT INTO `admin` (`id_admin`, `username`, `password`) VALUES
 (1, 'roichan', '12345'),
-(2, 'admin2', '$2y$10$YaNj7UgvntOXeqQ9o1TSKOnS6AOerX9UruDBE6bCF3kM4g1FcrZwi'),
-(3, 'shin', '$2y$10$SLJzdpgZIf8i0q5LGNbKkumw0JqireezObNDLz7UV.CUlW3yyqnrm');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `artikel`
---
-
-CREATE TABLE `artikel` (
-  `ID_Artikel` int(11) NOT NULL,
-  `Judul` varchar(100) NOT NULL,
-  `Isi` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `artikel`
---
-
-INSERT INTO `artikel` (`ID_Artikel`, `Judul`, `Isi`) VALUES
-(3, 'Sunat', '<p>Wajib</p>'),
-(4, 'artikel 2', '<p>weqweqwqwrqwrqw</p>'),
-(5, 'artikel 3', '<p>tawdawifahwgawgwa</p>'),
-(6, 'jacob', '<p>hita</p>');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `komentar`
---
-
-CREATE TABLE `komentar` (
-  `ID_Komentar` int(11) NOT NULL,
-  `Artikel` int(11) NOT NULL,
-  `Nama` varchar(50) NOT NULL,
-  `Isi` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `komentar`
---
-
-INSERT INTO `komentar` (`ID_Komentar`, `Artikel`, `Nama`, `Isi`) VALUES
-(1, 0, 'adam', 'Babi');
-
--- --------------------------------------------------------
-
---
--- Struktur dari tabel `user`
---
-
-CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data untuk tabel `user`
---
-
-INSERT INTO `user` (`id`, `username`, `password`) VALUES
-(2, 'admin', '$2y$10$/TjASjvOIORPQpTdnDRn5OWT4LF8ziEPLKtlAk8Yxr2'),
-(3, 'ican', '$2y$10$.jSonRYgkot8GFg/hKmfBe4X/qLQBMYQmsqWLKoWD2R'),
-(4, 'adam', '$2y$10$6hTO69nMBdjQ8cNNlAqhwO9B/xKS7Vgt66KldOQFGF.'),
-(5, 'admin1', '$2y$10$qz5UYN2ji9nLf/ts/SgbKeroR/WNGmSRpq8fhWTGYTR');
+(8, 'admin', 'admin');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `admin`
+-- Indexes for table `admin`
 --
 ALTER TABLE `admin`
   ADD PRIMARY KEY (`id_admin`);
 
 --
--- Indeks untuk tabel `artikel`
---
-ALTER TABLE `artikel`
-  ADD PRIMARY KEY (`ID_Artikel`);
-
---
--- Indeks untuk tabel `komentar`
---
-ALTER TABLE `komentar`
-  ADD PRIMARY KEY (`ID_Komentar`);
-
---
--- Indeks untuk tabel `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `admin`
+-- AUTO_INCREMENT for table `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT untuk tabel `artikel`
---
-ALTER TABLE `artikel`
-  MODIFY `ID_Artikel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- AUTO_INCREMENT untuk tabel `komentar`
---
-ALTER TABLE `komentar`
-  MODIFY `ID_Komentar` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT untuk tabel `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-COMMIT;
-
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
